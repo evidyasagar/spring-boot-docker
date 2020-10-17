2327a1ea55c38b462d1c4603d66a15e59e91804d
vidyaelluru <vidyaelluru@gmail.com>
2020-10-17 15:42:46 +0530

    spring-boot

---
+++ src/test/java/com/example/helloworld/springbootdocker/SpringBootDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helloworld.springbootdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
